0df5166677ff6fd3a77733e88333d03f1ae62bca
Krishna Kalyan Reddy Sabbella <kalyan_sabbella@outlook.com>
2022-04-22 23:55:13 -0400

    valid until change

--- StudentDataWarehouse/School DWH/Tables/Facts/FactRegionalStats.sql
+++ StudentDataWarehouse/School DWH/Tables/Facts/FactRegionalStats.sql
@@ -4,7 +4,7 @@
 	[RegionId] INT NOT NULL,
 	[SchoolId] INT NOT NULL,
 	[StudentCount] INT NOT NULL,
-	[StatsExpired] BIT NOT NULL DEFAULT 0,
+	[ValidUntil] DATETIME NOT NULL,
 	CONSTRAINT [FK_FactRegionalStats_Region_RegionId] FOREIGN KEY([RegionId]) REFERENCES DimRegion([RegionId]),
 	CONSTRAINT [FK_FactRegionalStats_School_SchoolId] FOREIGN KEY([SchoolId]) REFERENCES DimSchool([SchoolId]),
 )
