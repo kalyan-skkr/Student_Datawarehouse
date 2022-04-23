5e53e35685f05e54202b2e654e49e48e6a384564
Krishna Kalyan Reddy Sabbella <kalyan_sabbella@outlook.com>
2022-04-22 23:37:32 -0400

    School name

--- StudentDataWarehouse/School DWH/Tables/Dimensions/DimStudent.sql
+++ StudentDataWarehouse/School DWH/Tables/Dimensions/DimStudent.sql
@@ -7,6 +7,7 @@
 	[LastName] VARCHAR(20) NOT NULL,
 	[FullName] VARCHAR(100) NOT NULL,
 	[Gender] VARCHAR(1) NOT NULL,
+	[SchoolName] VARCHAR(30) NOT NULL,
 	[DateOfBirth] DATETIME NOT NULL,
 	[DateOfAdmission] DATETIME NOT NULL,
 	[DataValid] BIT NOT NULL DEFAULT 1
